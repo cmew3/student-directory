683040f444805e6f478edf3b90b4b3051f0c3d2b
cmew3 <c.wheeler-booth@cantab.net>
2014-06-26 18:51:41 +0100

    Some refactoring

--- directory2.rb
+++ directory2.rb
@@ -1,43 +1,54 @@
 $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 @student_list = []
 
-def get_students
-	puts "Hello! Let's make a directory of students"
+def add_students
 	puts "Please enter the student name."
-	name = gets.strip
+	name = STDIN.gets.chomp
 	while !name.empty?
 		puts "Which cohort is #{name} in?"
-		cohort = gets.strip.downcase
-		cohort = check_cohort cohort
+		cohort = input_valid_cohort
 		puts "What is #{name}'s hobby?"
-		hobby = gets.strip
+		hobby = STDIN.gets.chomp
 		puts "What is #{name}'s country of birth?"
-		country = gets.strip
+		country = STDIN.gets.chomp
 		@student_list << {:name => name, :cohort =>  cohort,:hobby => hobby, :country => country}
-		if @student_list.length <= 1
-			puts "There is now #{@student_list.length} student in our directory. Add another student or Enter twice to exit"
-		else
-			puts "There are now #{@student_list.length} students in our directory. Add another student or Enter twice to exit"
-		end
-		name = gets.strip
+		puts "There is now #{@student_list.length} student#{s_if_plural(@student_list)} in our directory. Add another student or Enter twice to exit"
+		name = STDIN.gets.chomp
 	end
 	@student_list
 end
 
-def check_cohort cohort_input
-	while !$months.include?(cohort_input)
+def input_valid_cohort
+	cohort = STDIN.gets.chomp.downcase
+	while !$months.include?(cohort)
 		puts 'That doesn\'t look like a month, please enter a month'
-		cohort_input = gets.strip.downcase
+		cohort = STDIN.gets.chomp.downcase
 	end
-	cohort_input.to_sym
+	cohort.to_sym
+end
+
+def s_if_plural array
+	if array.length <2
+		return ""
+	else
+		return "s"
+end
+	
+end
+
+def print_header
+	puts ""
+	puts "The students at Maker's Academy are:"
+	puts "============================================"
+	puts ""
 end
 
 def print_students
-	# student_list.sort_by! {|student| student[:cohort]}
 	$months.each do |month|
 		student_list_by_month = @student_list.select {|student| student[:cohort]==month.to_sym}
 		if student_list_by_month.length > 0
 			puts "#{month.capitalize} cohort"
+			puts "-"*(month.length + 7)
 			student_list_by_month.each_with_index do |student,index|
 				puts "#{index+1}. #{student[:name]} comes from #{student[:country]} and enjoys #{student[:hobby]}"
 			end
@@ -46,21 +57,18 @@ def print_students
 	end
 end
 
-def print_header
-	puts "The students at Maker's Academy are:"
-end
-
 def print_footer
 	if @student_list.length <= 1
-		puts "There is #{@student_list.length} lovely student in our cohort!"
+		puts "There is #{@student_list.length} lovely student in our directory!"
 	else
-		puts "There are #{@student_list.length} lovely students in our cohort!"
+		puts "There are #{@student_list.length} lovely students in our directory!"
 	end
 end
 
 def print_menu
 	puts ""
 	puts 'Please choose from the following options:'
+	puts ""
 	puts '1. Input the students'
 	puts '2. Show the students'
 	puts '3. Save the students to file students.csv'
@@ -71,9 +79,7 @@ end
 def show_students
 	if @student_list.length >0
 		print_header
-		puts ""
 		print_students
-		puts ""
 		print_footer
 	else
 		puts 'No students to show, try adding some students!'
@@ -83,10 +89,8 @@ end
 def process(choice)
 	case choice
 		when '1'
-			#input students
-			@student_list = get_students
+			@student_list = add_students
 		when '2'
-			#show students
 			show_students
 		when '3'
 			save_students
@@ -97,16 +101,16 @@ def process(choice)
 			exit
 		else
 			puts "I don't know what you meant, please type a number to choose from our menu."
-	end #end case
+	end 
 end
 
 def interactive_menu
 	puts 'Welcome to our directory.'
 	loop do
 		print_menu
-		process(gets.chomp)
-	end # end loop
-end #end menu def
+		process(STDIN.gets.chomp)
+	end 
+end 
 
 def save_students
 	#open the file for writing
@@ -119,7 +123,20 @@ def save_students
 	file.close
 end
 
-def load_students (filename = "students.csv")
+def valid_file
+	ARGV.first == nil ? filename = "students.csv" : filename = ARGV.first
+	puts filename
+	puts File.exists?(filename)
+	if File.exists?(filename)
+		return filename
+	else
+		puts "Sorry #{filename} name not recognised. Exiting program"
+		exit
+	end
+end
+
+def load_students
+	filename = valid_file
 	file = File.open(filename, "r")
 	file.readlines.each do |line|
 		name, cohort, hobby, country = line.chomp.split(',')
